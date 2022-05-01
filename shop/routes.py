ba3a2183a43a783f57385271f3f21175010d56c1
Dominik Alkhovik <dominik.alk@hotmail.com>
2022-05-01 03:45:17 +0100

    add cards to home page

--- shop/routes.py
+++ shop/routes.py
@@ -1,25 +1,55 @@
-from flask import render_template
+from flask import render_template, url_for
 from shop import app
 import sys
 
 @app.route("/")
-def home_page():
-    return render_template('index.html')
+def home():
+    posts = [
+        {
+            'name': 'Sandbox',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            'picture': 'https://venturebeat.com/wp-content/uploads/2021/03/article23-1.jpg?w=1200&strip=all',
+            'price': 20,
+            'carbon': 30
+        },
+        {
+            'name': 'Sandbox',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            'picture': 'https://venturebeat.com/wp-content/uploads/2021/03/article23-1.jpg?w=1200&strip=all',
+            'price': 20,
+            'carbon': 30
+        },
+        {
+            'name': 'Sandbox',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            'picture': 'https://venturebeat.com/wp-content/uploads/2021/03/article23-1.jpg?w=1200&strip=all',
+            'price': 20,
+            'carbon': 30
+        },
+        {
+            'name': 'Sandbox',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            'picture': 'https://venturebeat.com/wp-content/uploads/2021/03/article23-1.jpg?w=1200&strip=all',
+            'price': 20,
+            'carbon': 30
+        },
+    ]
+    return render_template('index.html', posts=posts)
 
 @app.route("/login")
-def login_page():
+def login():
     return render_template('login.html')
 
 @app.route("/register")
-def register_page():
+def register():
     return render_template('register.html')
 
 @app.route("/cart")
-def cart_page():
+def cart():
     return render_template('cart.html')
 
 @app.route("/checkout")
-def checkout_page():
+def checkout():
     return render_template('checkout.html')
 
 @app.errorhandler(404)
