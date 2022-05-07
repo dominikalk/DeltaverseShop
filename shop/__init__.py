9f9cb7feed3301d80836f0410e2507a62f616537
Dominik Alkhovik <dominik.alk@hotmail.com>
2022-05-07 07:01:20 +0100

    revert

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,9 +2,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 SECRET_KEY = os.urandom(32)
@@ -16,7 +16,7 @@ app = Flask(__name__, static_folder='static')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'shop.db')
 
 # Remote DB Environment
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c21024669:Rotate-Cow18@csmysql.cs.cf.ac.uk:3306/c21024669_deltaverse'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
